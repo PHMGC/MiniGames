6cfb7b8a7e326ee3b9c15f0e76a13cb7d53a43d5
Pedro Cortez <108164511+PHMGC@users.noreply.github.com>
2024-12-23 20:49:33 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,6 @@
 using namespace std;
 
 int main() {
-    //Código teste do SFML
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
-
-    sf::CircleShape shape(50);
-    shape.setFillColor(sf::Color::Green);
-
-    // Main loop
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        // Screen update
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
 
     return 0;
 }
