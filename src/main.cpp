2bf23c97aff87a218e0882896a42284c74e52490
PHMGC <108164511+PHMGC@users.noreply.github.com>
2024-12-24 22:44:00 -0300

    setting up project on clion

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,7 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace std;
+int main() {
 
- 
-int main()
-{
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
-
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        while (const std::optional event = window.pollEvent())
-        {
-            // Close window: exit
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
- 
-        // Clear screen
-        window.clear();
- 
-        // Draw the string
-        //window.draw(text);
- 
-        // Update the window
-        window.display();
-    }
+    return 0;
 }
